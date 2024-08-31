5613719c273c60df2eda654f983e8f07a6824b47
baluthripurari <baluthripurari@gmail.com>
2024-08-31 09:45:38 +0530

    message

--- output.tf
+++ output.tf
@@ -1,3 +1,22 @@
-output "azs" {
-  value = data.aws_availability_zones.available.names
+# output "azs" {
+#   value = data.aws_availability_zones.available.names
+# }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.Database[*].id
+}
+
+output "Database_subnet_ids" {
+  value = aws_subnet.Database[*].id
+}
+
+output "Database_subnet_group_ids" {
+  value = aws_db_subnet_group.default.id
 }
